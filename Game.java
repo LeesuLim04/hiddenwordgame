7002ec01f4e27860664cf599ed5bb6ebbba283e0
leesulim04 <llim@student-bmhs-la.org>
2023-01-08 20:55:25 -0800

    Completed HW

--- Game.java
+++ Game.java
@@ -1,17 +1,28 @@
+import java.util.Scanner;
 public class Game {
-    private String hidden;
-    private String guess;
-    private String hint;
     public static void main(String[] args) {
-        Game game = new Game();
-        game.run();
+        Scanner kb = new Scanner(System.in);
+        System.out.println("Enter a hidden word");
+        String hidden = kb.nextLine().toUpperCase();
+        String guess = "";
+
+        do {
+            System.out.println("Guess the hidden word");
+            guess = kb.nextLine().toUpperCase();
+            for (int i = 0; i < guess.length(); i++) {
+                String hint = "";
+                if (guess.substring(i, i + 1).equals(hidden.substring(i, i + 1))) {
+                    System.out.print(guess.substring(i, i + 1));
+                } else if (hidden.indexOf(guess.substring(i, i + 1)) > -1) {
+                    System.out.print("+");
+                } else {
+                    System.out.print("*");
+                }
+            }
+            System.out.println("");
+        }
+            while (!guess.equals(hidden));
+                System.out.println("Correct!");
+        }
     }
 
-    public void run() {
-        System.out.println("hello world");
-    }
-
-
-
-
-}
